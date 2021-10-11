c0d67a1fbd0a4d1b0126619b039982406d9fdb7d
Vitaliy Nichipienko <VitaliyNichipienko@users.noreply.github.com>
2021-10-11 22:16:57 +0700

    Реализован перевод потока в состояние ошибки при некорректном вводе времени

--- OOTRPO/AIRPLANE.cpp
+++ OOTRPO/AIRPLANE.cpp
@@ -24,36 +24,10 @@
 
 	void AIRPLANE::setDepartureTime(string departureTime)
 	{
-		while (true)
+		if (CheckTime(departureTime) == true)
 		{
-			vector<string> arr;
-			string inputStr = departureTime;
-			string delim = ":";
-			size_t prev = 0;
-			size_t next;
-			size_t delta = delim.length();
-
-			while ((next = inputStr.find(delim, prev)) != string::npos)
-			{
-				string tmp = inputStr.substr(prev, next - prev);
-				arr.push_back(inputStr.substr(prev, next - prev));
-				prev = next + delta;
-			}
-			arr.push_back(inputStr.substr(prev));
-
-			int watches = stoi(arr[0]);
-			int	minutes = stoi(arr[1]);
-
-			if (watches >= 24 || watches < 0 || minutes >= 60 || minutes < 0 || arr.size() != 2)
-			{
-				cout << "Please enter the correct time, time format - \"HH:MM\" " << endl;
-				cin >> departureTime;
-				continue;
-			}
-			break;
+			this->departureTime = departureTime;
 		}
-
-		this->departureTime = departureTime;
 	}
 
 	void AIRPLANE::setAirplaneType(string airplaneType)
@@ -81,6 +55,35 @@
 		return airplaneType;
 	}
 
+	bool AIRPLANE::CheckTime(string time)
+	{
+		vector<string> arr;
+		string inputStr = time;
+		string delim = ":";
+		size_t prev = 0;
+		size_t next;
+		size_t delta = delim.length();
+
+		while ((next = inputStr.find(delim, prev)) != string::npos)
+		{
+			string tmp = inputStr.substr(prev, next - prev);
+			arr.push_back(inputStr.substr(prev, next - prev));
+			prev = next + delta;
+		}
+		arr.push_back(inputStr.substr(prev));
+
+		int watches = stoi(arr[0]);
+		int	minutes = stoi(arr[1]);
+
+		if (watches >= 24 || watches < 0 || minutes >= 60 || minutes < 0 || arr.size() != 2)
+		{
+			cout << "Incorrect time entered" << endl;
+			return false;
+		}
+
+		return true;
+	}
+
 #pragma endregion
 
 
@@ -89,13 +92,22 @@
 
 	istream& operator >> (istream& in, AIRPLANE& airplane)
 	{
-		string str1, str2, str3, str4;
-		in >> str1 >> str2 >> str3 >> str4;
+		string destination, flightNumber, departureTime, airplaneType;
+		in >> destination >> flightNumber >> departureTime >> airplaneType;
 
-		airplane.setDestination(str1);
-		airplane.setFlightNumber(str2);
-		airplane.setDepartureTime(str3);
-		airplane.setAirplaneType(str4);
+
+		if (airplane.CheckTime(departureTime) == true)
+		{
+			airplane.setDestination(destination);
+			airplane.setFlightNumber(flightNumber);
+			airplane.setDepartureTime(departureTime);
+			airplane.setAirplaneType(airplaneType);
+		}
+		else
+		{
+			in.clear(ios::failbit);
+			return in;
+		}
 
 		return in;
 	}
